0afa23488d10361f9ee3eabb20ac65e3dee923a7
fu0s <ela.soufine@gmail.com>
2020-03-29 16:21:47 +0200

    generated deployables

--- docs/main.js
+++ docs/main.js
@@ -144,7 +144,7 @@ module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<mat-card>Simple card</mat-card>\n"
+module.exports = "<mat-card class = \"tp-card\">\n  <mat-card-header>\n     <div mat-card-avatar class = \"tp-header-image\"></div>\n     <mat-card-title>HTML5</mat-card-title>\n     <mat-card-subtitle>HTML Basics</mat-card-subtitle>\n  </mat-card-header>\n  <img mat-card-image src = \"https://www.tutorialspoint.com/materialize/src/html5-mini-logo.jpg\" alt = \"Learn HTML5\">\n  <mat-card-content>\n     <p>\n        HTML5 is the next major revision of the HTML standard superseding\n        HTML 4.01, XHTML 1.0, and XHTML 1.1. HTML5 is a standard for\n        structuring and presenting content on the World Wide Web.\n     </p>\n  </mat-card-content>\n  <mat-card-actions>\n     <button mat-button>LIKE</button>\n     <button mat-button>SHARE</button>\n  </mat-card-actions>\n</mat-card>"
 
 /***/ }),
 
